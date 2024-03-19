faee48173211710be7955bb3ee07b756228502c5
Sierraaa <143159653+asierra619@users.noreply.github.com>
2024-03-19 16:18:45 -0700

    added wishlist model

--- server/models/wishlist.js
+++ server/models/wishlist.js
@@ -1,6 +1,20 @@
-// wishlist: {
+const { Schema, model } = require('mongoose');
+
+
+// wishlist model {
 //   _id!
-//   pizza: [Pizza]
-//   otherItems: [OtherItems]
+//   food: [Food]
 //   }
-  
+
+const wishlistSchema = new Schema({
+  food: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'food'
+    }
+  ]
+});
+
+const Wishlist = model('wishlist', wishlistSchema);
+
+module.exports = Wishlist;

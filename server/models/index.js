5601205a0dc817c512c01bb8cebd23ce6265b2a6
Sierraaa <143159653+asierra619@users.noreply.github.com>
2024-03-21 20:14:40 -0700

    name change

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
 const User = require('./user');
 const Food = require('./food');
-const Category = require('./category1');
+const Category = require('./category');
 //const Cart = require('./cart');
 //const Wishlist = require('./wishlist');
 

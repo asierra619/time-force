f054074bfb82defbd10f172826c65d210a335a43
Sierraaa <143159653+asierra619@users.noreply.github.com>
2024-03-21 19:47:20 -0700

    removed type and added vite scripts to packagejson, added temporary div to appJSX, and edited minor typos causing bugs in typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -40,7 +40,7 @@ type Auth {
 
 type Query {
     me: User
-    allCategory: Catergory
+    allCategory: Category
     allFood: Food
     allUsers: User
 }
@@ -48,11 +48,11 @@ type Query {
 type Mutation {
     createUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    saveToCart(args): User
+    saveToCart(args: ID): User #added ID to debug - change as needed
     deleteFromCart(foodName: String!): User
-    saveToWishlish(args): User
+    saveToWishlist(args: ID): User #added ID to debug - change as needed
     deleteFromWishlist(foodName: String!): User
- }
+}
 `;
 
 module.exports = typeDefs;

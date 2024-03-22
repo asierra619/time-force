fff307fc6843c8c828c1093d2029c4fdec7211a4
ylf456 <145236890+ylf456@users.noreply.github.com>
2024-03-21 17:05:08 -0700

    added dummy data (seeds) for MongoDB

--- server/seeder/seed.js
+++ server/seeder/seed.js
@@ -1,7 +1,6 @@
 const db = require('../config/connection');
-const { User, Thought } = require('../models');
-const userSeeds = require('./userSeeds.json');
-const thoughtSeeds = require('./thoughtSeeds.json');
+const { User, Category, Food } = require('../models');
+const { userSeeds, categorySeeds, foodSeeds } = require('./seeds/index.js')
 const cleanDB = require('./cleanDB');
 
 // cleanDB clear the records in the collections of the database
@@ -9,12 +8,12 @@ db.once('open', async () => {
   try {
     // cleandb = async (modelName, collectionName) =>{}
     await cleanDB('User', 'users');
-
     await cleanDB('Category', 'categories');
-
     await cleanDB('Food','food')
 
     await User.create(userSeeds);
+    await Category.create(categorySeeds);
+    await Food.create(foodSeeds);
 
     for (let i = 0; i < thoughtSeeds.length; i++) {
       const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);

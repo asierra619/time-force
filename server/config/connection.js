dc0e450272fd8c0e289e47f844cd517fa4cc6a20
ylf456 <145236890+ylf456@users.noreply.github.com>
2024-03-25 21:17:57 -0700

    staging commit

--- server/config/connection.js
+++ server/config/connection.js
@@ -5,3 +5,16 @@ connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
 // mern-shopping is the name of the local db
 
 module.exports = connection;
+
+
+
+/* //local host
+require("dotenv").config();
+const { connect, connection } = require('mongoose');
+
+connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
+// mern-shopping is the name of the local db
+
+module.exports = connection;
+
+*/
